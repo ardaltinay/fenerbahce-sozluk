c82949553989eed5dd3b29a069744df7ad959b99
ardaltinay <arda.altinay@ozguryazilim.com.tr>
2025-12-24 00:02:24 +0300

    fix

--- server/src/main/java/net/fenerbahcesozluk/repository/TopicRepository.java
+++ server/src/main/java/net/fenerbahcesozluk/repository/TopicRepository.java
@@ -9,12 +9,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface TopicRepository extends JpaRepository<Topic, UUID> {
 
-  java.util.Optional<Topic> findByTitleIgnoreCase(String title);
+  Optional<Topic> findByTitleIgnoreCase(String title);
 
   Page<Topic> findByIsActiveTrueOrderByCreatedAtDesc(Pageable pageable);
 
